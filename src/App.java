85962ca4f8f8b85460755ae706ebe31a81110492
Douaa <dgoumdi@hawkmail.hfcc.edu>
2025-11-15 13:00:04 -0500

    initialize the main class

--- src/App.java
+++ src/App.java
@@ -5,6 +5,29 @@ import java.io.FileNotFoundException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Astronaut astronaut = new Astronaut();
+        ArrayList<Planets> planetsList = new ArrayList<Planets>();
+        System.out.println("=====================================");
+        System.out.println("      Space Expedition WitVerse      ");
+        System.out.println("=====================================");
+        System.out.println("Hello Astronaut! Welcome to your space adventure.\n TO CONTINUE, ENTER YOUR NAME: ");
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        astronaut.setName(name);
+
+        System.out.println("Welcome to Space Expedition WitVerse.\n 
+        Your mission is to navigate through the stars and explore new worlds!\n
+        Good luck, " + name + "!\n Type 'start' to begin your adventure.");
+        String command = scanner.nextLine();
+        if (command.equalsIgnoreCase("start")) {
+            System.out.println("you have started your adventure, " + astronaut.getName() + "!");
+            System.out.println("Now you are in the Milky Way galaxy, home to Planet Earth - the best planet ever.\n" +
+            "From here, your next stop will be Aether-1, shimmering world of floating islands , each with its own unique challenges and rewards");
+        } else {
+            System.out.println("Invalid command. Please restart the application to try again.");
+        }
+        
     }
 }
