c2aa6317b6aba8c8859f9ec51e9b8f1fe18ebbc5
annapham042@gmail.com <annapham042@gmail.com>
2025-11-15 12:08:46 -0500

    finished up astronaut and imported some unitilies to main so we can scan some files

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
--- src/Astronaut.java
+++ src/Astronaut.java
@@ -1,3 +1,49 @@
 public class Astronaut {
 
+    private String name;
+    private int reward;
+
+    public Astronaut(String name, int reward) {
+        super();
+        this.name = name;
+        this.reward = reward;
+
+    }
+
+    public Astronaut() {
+        super();
+        this.name = "unknown";
+        this.reward = 0;
+
+    }
+
+    public Astronaut(int reward) {
+        super();
+        this.name = name;
+        this.reward = 0;
+
+    }
+
+    public Astronaut(String name) {
+        super();
+        this.name = "unknown";
+        this.reward = reward;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
 }
